136901e717a9d74d17715239bf27ee7918bb7ae1
Greyun <yhy3587@gmail.com>
2022-07-29 17:27:15 +0900

    modify initial config and update hello-world contents

--- blog-config.js
+++ blog-config.js
@@ -1,17 +1,15 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  title: "Greyun's blog",
+  description: "This is greyun's develop blog",
+  author: "greyun",
+  siteUrl: "https://greyun.com",
   links: {
-    github: "https://github.com/devHudi",
-    linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com",
-    instagram: "https://www.instagram.com",
-    email: "mailto:devhudi@gmail.com",
+    github: "https://github.com/Greyun",
+    linkedIn: "https://www.linkedin.com/in/hyunyoung-yun-8366a2236",
+    email: "mailto:yhy3587@gmail.com",
   },
   utterances: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "Greyun/dev-blog",
     type: "pathname",
   },
 }
---
+++ contents/posts/hello-world/index.md
@@ -0,0 +1,13 @@
+---
+title: "Hello World!"
+description: "This is greyun's blog first contents"
+date: 2022-07-29
+update: 2022-07-29
+tags:
+  - about
+series:
+---
+
+## Hello World Blog!
+
+이 블로그는 gatsby-starter-hoodie 로 작성되었습니다.
